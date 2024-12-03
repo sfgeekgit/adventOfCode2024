bf2ac652dd04edd834fe04cd3fe3f02cb65bcece
sfgeekgit <sfgeek@gmail.com>
2024-12-03 15:31:02 -0800

    Second puzzle of 2024

--- 02.py
+++ 02.py
@@ -1,6 +1,8 @@
 # Just a quick one off the get the answer
 # Nothing fancy, skipping input checks, etc.
 
+from collections import Counter
+
 filename = "input01.txt"
 
 list1 = []
@@ -15,10 +17,13 @@ with open(filename, "r") as file:
 #list1 = [3,4,2,1,3,3]
 #list2 = [4,3,5,3,9,3]
 
-list1.sort()
-list2.sort()
-dist = 0
-for i in range(len(list1)):
-    dist += abs(list1[i] - list2[i])
+#list1.sort()
+#list2.sort()
 
-print(dist)
+cnt2 = Counter(list2)
+
+sim = 0
+for i in range(len(list1)):
+    sim += list1[i] * cnt2[list1[i]]
+
+print(sim)
